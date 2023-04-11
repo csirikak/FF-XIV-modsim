21cc97a77626246ee25579f9eb2d42b648434f50
nj <csirikak@stevens.edu>
2023-04-11 18:42:00 -0400

    Working prototype

--- playerList/playerList.cpp
+++ playerList/playerList.cpp
@@ -1,56 +1,127 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <functional>
+#include <sstream>
+#include <iomanip>
+#include <cstdint>
 
+const unsigned char empty_line[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+
+ULONGLONG parseHexArgument(const char* arg)
+{
+    // Create an input string stream from the argument
+    std::istringstream iss(arg);
+
+    // Read the value as a hexadecimal number
+    ULONGLONG value;
+    iss >> std::hex >> value;
+
+    // Check for errors during parsing
+    if (iss.fail() || !iss.eof()) {
+        throw std::invalid_argument("Invalid hexadecimal string: " + std::string(arg));
+    }
+
+    // Return the parsed value
+    return value;
+}
+
+// Example usage
+
+BOOL SetPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege) {
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+
+    if (!LookupPrivilegeValue(NULL, lpszPrivilege, &luid)) {
+        return FALSE;
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = bEnablePrivilege ? SE_PRIVILEGE_ENABLED : 0;
+
+    if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL)) {
+        return FALSE;
+    }
+
+    if (GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
+void printList(HANDLE hProcess, ULONGLONG memAddressLocation){
+    char* pBuffer = new char[32768];
+    SIZE_T bytesRead = 0;
+    std::cout << "Printing list at: 0x" << std::hex << std::setw(16) << std::setfill('0') << memAddressLocation << std::endl;
+    if (ReadProcessMemory(hProcess, (LPCVOID)memAddressLocation, pBuffer, 32768, &bytesRead)) {
+        for (ULONGLONG i = 21; i < bytesRead; i++) {
+            char* outputPtr = &pBuffer[i];
+            if (!memcmp(&pBuffer[i], empty_line, 16)){
+                i = bytesRead;
+                std::cout << "EOL";
+            }
+            else {
+                while (*outputPtr != '\0')
+                {
+                    std::cout << *outputPtr;
+                    outputPtr++;
+                }
+                std::cout << std::endl;
+                i+=95;
+            }
+        }
+    }
+    else {
+        std::cout << "Error: ReadProcessMemory failed, Error code: " << GetLastError() << std::endl;
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        std::cout << "Usage: " << argv[0] << " <program name> <string>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <process ID> <memory address>" << std::endl;
         return 1;
     }
 
     DWORD pid = std::atoi(argv[1]);
-    std::string str(argv[2]);
 
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-    if (hProcess == NULL) {
-        std::cout << "Error: OpenProcess failed" << std::endl;
+    HANDLE hToken = NULL;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+        std::cout << "Error: OpenProcessToken failed, Error code: " << GetLastError() << std::endl;
         return 1;
     }
 
-    SYSTEM_INFO sysInfo;
-    GetSystemInfo(&sysInfo);
-
-    MEMORY_BASIC_INFORMATION64 memInfo;
-    ZeroMemory(&memInfo, sizeof(MEMORY_BASIC_INFORMATION64));
-
-    ULONGLONG dwAddress = 0;
-    while (dwAddress < (ULONGLONG)sysInfo.lpMaximumApplicationAddress) {
-        if (VirtualQueryEx(hProcess, (LPCVOID)dwAddress, (PMEMORY_BASIC_INFORMATION)&memInfo, sizeof(MEMORY_BASIC_INFORMATION64)) == sizeof(MEMORY_BASIC_INFORMATION64)) {
-            if ((memInfo.State == MEM_COMMIT) && (memInfo.Type == MEM_PRIVATE)) {
-                char* pBuffer = new char[memInfo.RegionSize];
-                SIZE_T bytesRead = 0;
-                if (ReadProcessMemory(hProcess, (LPCVOID)memInfo.BaseAddress, pBuffer, memInfo.RegionSize, &bytesRead)) {
-                    for (ULONGLONG i = 0; i < bytesRead; i++) {
-                        if (memcmp(&pBuffer[i], str.c_str(), str.length()) == 0) {
-                            std::cout << "Found string at address: 0x" << std::hex << ((ULONGLONG)memInfo.BaseAddress + i) << std::endl;
-                            char* outputPtr = &pBuffer[i];
-                            while (*outputPtr != '\0')
-                            {
-                                std::cout << *outputPtr;
-                                outputPtr++;
-                            }
-                            std::cout << std::endl;
-    
-                        }
-                    }
-                }
-                delete[] pBuffer;
-            }
-        }
-        dwAddress = (ULONGLONG)memInfo.BaseAddress + memInfo.RegionSize;
+    if (!SetPrivilege(hToken, SE_DEBUG_NAME, TRUE)) {
+        std::cout << "Error: SetPrivilege failed, Error code: " << GetLastError() << std::endl;
+        CloseHandle(hToken);
+        return 1;
     }
+    CloseHandle(hToken);
 
+
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (hProcess == NULL) {
+        std::cout << "Error: OpenProcess failed, Error code: " << GetLastError() << std::endl;
+        return 1;
+    }
+    try {
+    // Parse the argument as an ULONGLONG
+        ULONGLONG value = parseHexArgument(argv[2]);
+        printList(hProcess, value);
+    } 
+    catch (const std::exception& e) {
+        // Print the error message
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+ 
     CloseHandle(hProcess);
 
     return 0;
